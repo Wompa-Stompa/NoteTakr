13fc6393362a5fe47a0f5e4161b829885716fa08
Wompa-Stompa <chavothewonderdog@gmail.com>
2022-09-22 11:50:01 -0500

    cant fix Get route

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 An app to help you take notes, edit them and delete them when you're done. 
+Made using Express, MongoDB, and Mongoose 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = function(app) {
   app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"))
+    res.sendFile(path.join(__dirname, "./notes.html"))
   })
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"))
+    res.sendFile(path.join(__dirname, "./public/index.html"))
   })
 }
